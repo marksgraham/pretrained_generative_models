dc5f6410b1fd5d959660fe472ff4cf03a62b28a7
Mark Graham <markgraham539@gmail.com>
2023-01-11 11:52:59 -0600

    Initial commit

---
+++ hubconf.py
@@ -0,0 +1,4 @@
+# Optional list of dependencies required by the package
+dependencies = ["torch", "generative"]
+
+from pretrained_generative_models.ddpm_2d import ddpm_2d
---
+++ models/ddpm_2d.py
@@ -0,0 +1,50 @@
+import errno
+import os
+from typing import Optional
+
+import gdown
+import torch
+import torch.nn as nn
+from torch import Tensor
+from generative.networks.nets import DiffusionModelUNet
+
+def ddpm_2d(
+    model_dir: Optional[str] = None,
+    file_name: str = "ddpm_2d.pth",
+    progress: bool = True,
+):
+    if model_dir is None:
+        hub_dir = torch.hub.get_dir()
+        model_dir = os.path.join(hub_dir, "checkpoints")
+
+    try:
+        os.makedirs(model_dir)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            # Directory already exists, ignore.
+            pass
+        else:
+            # Unexpected OSError, re-raise.
+            raise
+
+    filename = file_name
+    cached_file = os.path.join(model_dir, filename)
+    if not os.path.exists(cached_file):
+        gdown.download(
+            url="https://drive.google.com/uc?export=download&id=1j9lRK-D7enXtywIhzQwdaxPKy3AWObwd",
+            output=cached_file,
+            quiet=not progress,
+        )
+
+    model = DiffusionModelUNet(
+        spatial_dims=2,
+        in_channels=1,
+        out_channels=1,
+        num_channels=(128, 256, 256),
+        attention_levels=(False, True, True),
+        num_res_blocks=1,
+        num_head_channels=256,
+    )
+    model.load_state_dict(torch.load(cached_file, map_location='cpu'))
+    return model
+	
