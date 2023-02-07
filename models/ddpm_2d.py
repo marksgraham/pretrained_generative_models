91480d1b92f1310cb82f7d016739b3bbbd5e44f0
Mark Graham <markgraham539@gmail.com>
2023-02-07 11:14:17 -0600

    Add new model

--- models/ddpm_2d.py
+++ models/ddpm_2d.py
@@ -48,3 +48,42 @@ def ddpm_2d(
     model.load_state_dict(torch.load(cached_file, map_location='cpu'))
     return model
 	
+def ddpm_2d_v2(
+    model_dir: Optional[str] = None,
+    file_name: str = "ddpm_2d.pth",
+    progress: bool = True,
+):
+    if model_dir is None:
+        hub_dir = torch.hub.get_dir()
+        model_dir = os.path.join(hub_dir, "checkpoints")
+
+    try:
+        os.makedirs(model_dir)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            # Directory already exists, ignore.
+            pass
+        else:
+            # Unexpected OSError, re-raise.
+            raise
+
+    filename = file_name
+    cached_file = os.path.join(model_dir, filename)
+    if not os.path.exists(cached_file):
+        gdown.download(
+            url="https://drive.google.com/uc?export=download&id=1UTaeBMHIFxQDxWMoQNqSffGhbUDVRatZ",
+            output=cached_file,
+            quiet=not progress,
+        )
+
+    model = DiffusionModelUNet(
+        spatial_dims=2,
+        in_channels=1,
+        out_channels=1,
+        num_channels=(128, 256, 256),
+        attention_levels=(False, True, True),
+        num_res_blocks=1,
+        num_head_channels=256,
+    )
+    model.load_state_dict(torch.load(cached_file, map_location='cpu'))
+    return model
